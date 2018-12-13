d5360430864a1c9ec8526719dc24acefac79d7e6
wangchen93 <85513041@qq.com>
2018-12-13 15:20:05 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import MintUi from "mint-ui"
+import "mint-ui/lib/style.css"
+import "./assets/mui/css/mui.min.css"
+import "bootstrap/dist/css/bootstrap.css"
 
-Vue.config.productionTip = false
+Vue.use(MintUi)
 
 new Vue({
+  el:'#app',
   render: h => h(App),
-}).$mount('#app')
+})
