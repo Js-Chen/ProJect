059936e7bb9469584bed678bf49990678d93b47e
wangchen93 <85513041@qq.com>
2018-12-13 15:21:31 +0800

    vue环境搭建

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import MintUi from "mint-ui"
 import "mint-ui/lib/style.css"
 import "./assets/mui/css/mui.min.css"
 import "bootstrap/dist/css/bootstrap.css"
-
 Vue.use(MintUi)
-
 new Vue({
   el:'#app',
   render: h => h(App),
